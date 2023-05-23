67a6267295a6c1e91ccbfcbacbb81ea640a844c7
zmardil <btzardil@gmail.com>
2023-05-24 03:03:09 +0530

    Add Eureka server for service discovery

--- service-discovery/src/main/java/me/zmardil/servicediscovery/App.java
+++ service-discovery/src/main/java/me/zmardil/servicediscovery/App.java
@@ -1,11 +1,11 @@
-package me.zmardil.customerservice;
+package me.zmardil.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class App {
 
 	public static void main(String[] args) {
